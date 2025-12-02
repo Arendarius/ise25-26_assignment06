ad8617257a6a5e3cabc3a69d4d9b0ad02421130f
Arendarius <arendar.daniel@gmail.com>
2025-12-02 13:43:32 +0100

    mapper

--- api/src/main/java/de/seuhd/campuscoffee/api/mapper/UserDtoMapper.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/mapper/UserDtoMapper.java
@@ -1,5 +1,12 @@
 package de.seuhd.campuscoffee.api.mapper;
 
+import de.seuhd.campuscoffee.api.dtos.PosDto;
+import de.seuhd.campuscoffee.api.dtos.UserDto;
+
+@Mapper(componentModel = "spring")
+@ConditionalOnMissingBean // prevent IntelliJ warning about duplicate bean
 public interface UserDtoMapper {
     //TODO: Implement user DTO mapper
+    UserDto fromDomain(UserDto source);
+    UserDto toDomain(UserDto source);
 }
